e5dc5d59ee7f833c02f091717d332a2b56a37ddc
Adeleke Togun <adeleketogun@gmail.com>
2015-08-12 10:46:48 +0100

    UI fixes

--- js/agency.js
+++ js/agency.js
@@ -18,7 +18,32 @@ $(function() {
 // Highlight the top nav as scrolling occurs
 $('body').scrollspy({
     target: '.navbar-fixed-top'
-})
+});
+
+$(document).ready(function(){
+  $('#entry_1682894312').on('change', function(){
+    var chose = $(this).val();
+    var team = $('#entry_1035444962');
+    if(chose == 'Hackathon') {
+      team.removeAttr('disabled');
+    } else {
+      team.attr('disabled','disabled');
+    }
+  });
+});
+
+$('#ss-form').on('submit', function(e){
+  e.preventDefault();
+  var url = 'https://docs.google.com/forms/d/1Pc2pNL0XIl1lPG_oi3Nm7UbYwxs9IGirBkr7rlBKzPE/formResponse';
+  var data = $(this).serialize();
+  if( parseInt($("#entry_1682894312").val()) == 0) {
+    alert('Please select what you are registering for');
+  } else {
+    $.post(url, data, function(response) {
+    console.log(respnse);
+    });
+  }
+});
 
 // Closes the Responsive Menu on Menu Item Click
 $('.navbar-collapse ul li a').click(function() {
